222ae6d91fa29cbfcb749d617b327b3a62e2d9d1
Stephen John Raymundo <bayan.mahal@live.com>
2019-01-24 12:42:13 +0800

    Add files via upload

---
+++ precache-manifest.a8ebd181cf5c365c53c92d0a7d7b5325.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d9f3d46ed56f0d9d7b8d",
+    "url": "/js/chunk-vendors.8ef148a6.js"
+  },
+  {
+    "revision": "fb4d05da447df449aea6",
+    "url": "/js/app.23b76160.js"
+  },
+  {
+    "revision": "a2678d19c54cf647fb837430173c3ab7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb4d05da447df449aea6",
+    "url": "/css/app.b51c7f2f.css"
+  }
+];
